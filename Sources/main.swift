69d54d3027b8e58b9f9f1cf8121588cabdc4b28c
Joe Legner <joelegner@gmail.com>
2024-03-04 18:09:59 -0500

    Added a welcome message

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,4 +1,13 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+struct App {
+    static func main() {
+        divider()
+        print("Swift KitchenSink")
+        print("https://github.com/joelegner/swift-kitchen-sink")
+        print("This command-line interface (CLI) program demonstrates Swift features.\nIts source code is released with GPL-3.0 license, and it is heavily documented.")
+        divider()
+    }
+}
 
-print("Hello, world!")
+App.main()
