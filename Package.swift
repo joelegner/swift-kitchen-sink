7e77464d5577905e994bae32ff747ff6f46021ff
Joe Legner <joelegner@gmail.com>
2025-02-05 16:27:18 -0500

    Removes useless comment

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "KitchenSink",
     platforms: [
-        .macOS(.v10_15)  // Specify macOS 10.15 as the minimum deployment target
+        .macOS(.v10_15)
     ],
     targets: [
         .executableTarget(
