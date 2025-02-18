0b53adca19b2547d9cce8bdf520820e2be6d8898
Joe Legner <joelegner@gmail.com>
2025-02-18 17:54:49 -0500

    Starts building a better tree

--- Sources/CodeExamples/WeightedTree/WeightedTreeExample.swift
+++ Sources/CodeExamples/WeightedTree/WeightedTreeExample.swift
@@ -8,14 +8,23 @@ import Foundation
 
 struct WeightedTreeExample: CodeExample {
     func run() {
-        let tree: WeightedTree = WeightedTree("Live life", value: 24*60)
         print(#fileID, #function)
-        _ = tree.root.addChild(Node("Be awake", weight: 2))
+        let tree: WeightedTree = WeightedTree("Live life", value: 24*60)
+        var beAwake: Node 
+        var doThings: Node
+
+        beAwake = tree.root.addChild(Node("Be awake", weight: 2))
         _ = tree.root.addChild(Node("Sleep", weight: 1))
-        _ = tree.root.children[0].addChild(Node("Grandchild 1", weight: 4))
-        _ = tree.root.children[0].addChild(Node("Grandchild 2", weight: 5))
-        _ = tree.root.children[1].addChild(Node("Grandchild 3", weight: 6))
-        _ = tree.root.children[1].addChild(Node("Grandchild 4", weight: 7))
+        _ = beAwake.addChild(Node("Rest and Relax", weight:1))
+        doThings = beAwake.addChild(Node("Do things", weight:1))
+        _ = doThings.addChild(Node("Work", weight: 8))
+        _ = doThings.addChild(Node("Play", weight: 1))
+        _ = doThings.addChild(Node("Eat", weight: 2))
+        _ = doThings.addChild(Node("Exercise", weight: 1))
+        _ = doThings.addChild(Node("Learn", weight: 1))
+        _ = doThings.addChild(Node("Create", weight: 1))
+        _ = doThings.addChild(Node("Socialize", weight: 1))
+        
         print(tree.root)
     }
 }
