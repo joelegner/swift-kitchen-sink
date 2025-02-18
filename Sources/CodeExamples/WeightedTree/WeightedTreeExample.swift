eed4f07a619af8ec3d6518232e102402f7bd255b
Joe Legner <joelegner@gmail.com>
2025-02-18 18:08:33 -0500

    Adds sleep activities

--- Sources/CodeExamples/WeightedTree/WeightedTreeExample.swift
+++ Sources/CodeExamples/WeightedTree/WeightedTreeExample.swift
@@ -8,15 +8,25 @@ import Foundation
 
 struct WeightedTreeExample: CodeExample {
     func run() {
-        print(#fileID, #function)
-        let tree: WeightedTree = WeightedTree("Live life", value: 24*60)
+        print(#fileID, #function, "\n")
         var beAwake: Node 
         var doThings: Node
+        var sleep: Node
 
+        // Level 0: Root
+        let tree: WeightedTree = WeightedTree("LIVE ONE DAY", value: 24*60)
+
+        // Level 1
         beAwake = tree.root.addChild(Node("Be awake", weight: 2))
-        _ = tree.root.addChild(Node("Sleep", weight: 1))
-        _ = beAwake.addChild(Node("Rest and Relax", weight:1))
-        doThings = beAwake.addChild(Node("Do things", weight:1))
+        sleep = tree.root.addChild(Node("Sleep", weight: 1))
+
+        // Level 2
+        _ = beAwake.addChild(Node("Rest and Relax", weight:2))
+        sleep.addChildren([ Node("Sleep at night", weight: 7), Node("Take a nap", weight: 1)])
+        doThings = beAwake.addChild(Node("Do things", weight:3))
+    
+
+        // Level 3
         _ = doThings.addChild(Node("Work", weight: 8))
         _ = doThings.addChild(Node("Play", weight: 1))
         _ = doThings.addChild(Node("Eat", weight: 2))
